4309fdc5e817aed3f78c0f7954ea613098ca6148
Andrés  He <asiaticous@gmail.com>
2021-06-10 02:30:23 -0400

    ADD users scaffold and basics functions

---
+++ app/javascript/components/User.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+class User extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Name: {this.props.name}
+        Lastname: {this.props.lastname}
+        Email: {this.props.email}
+        Balance: {this.props.balance}
+      </React.Fragment>
+    );
+  }
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  lastname: PropTypes.string,
+  email: PropTypes.string,
+  balance: PropTypes.decimal
+};
+export default User
