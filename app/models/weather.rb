f6342e42cfd434b30660b2bf8c181910c01bd366
Andrés  He <asiaticous@gmail.com>
2021-06-11 03:55:40 -0400

    heroku

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,7 +3,7 @@ class Weather < ApplicationRecord
   def self.update_week
     today = Date.current
     url = "https://api.meteomatics.com/#{today.to_s}T16:00:00.000Z--#{(today + 1.week).to_s}T16:00:00.000Z:P1D/t_2m:C/-33.425596,-70.692112_-33.4785698,-70.6249963:0.1,0.1/json?model=mix"
-    weather_report = JSON.load(URI.open(url, :http_basic_authentication => [Rails.application.credentials[:meteomatics_user], Rails.application.credentials[:meteomatics_pass]]))["data"].first["coordinates"].first["dates"]
+    weather_report = JSON.load(URI.open(url, :http_basic_authentication => ["none_he", "aGxKis12X8gVQ"]))["data"].first["coordinates"].first["dates"]
     weather_report.each do |day|
      weather = self.find_or_initialize_by(date: day["date"].to_date)
      weather.temperature = day["value"]
